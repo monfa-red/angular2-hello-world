b38b1076f42486882115191444d9effbf1be1165
Abbas Monfared <a@monfa.red>
2015-07-21 17:51:06 +0000

    Initial commit :)

---
+++ main.ts
@@ -0,0 +1,23 @@
+/// <reference path="typings/angular2/angular2.d.ts" />
+
+import {Component, View, bootstrap} from 'angular2/angular2';
+
+// Anotation section
+@Component({
+	selector: 'my-app'
+})
+@View({
+	template: `<h1>Hello {{ name }}</h1>`,
+})
+
+// Component controller
+class MyAppComponent {
+	name: string;
+	
+	constructor() {
+		this.name = "World";
+	}
+}
+
+// Bootstrap!
+bootstrap(MyAppComponent);
